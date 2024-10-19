025bf11b539bf5c6b9737e98ef9924e431887afe
Gorav <goravjindal86@gmail.com>
2024-10-19 18:54:21 +0530

    dictionary app

--- src/scripts/dictionaryapi.js
+++ src/scripts/dictionaryapi.js
@@ -1,12 +1,3 @@
-/*******************************************************************************
- * dictionaryapi.js
- *
- * Interface to vendor https://dictionaryapi.dev
- *
- * Query using HTTP GET https://api.dictionaryapi.dev/api/v2/entries/en/keyboard
- *
- *******************************************************************************/
-
 async function dictionarySearch(term) {
   setBusy(true);
 
